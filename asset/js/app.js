2a973da8145b6bf7a193bfb9c0f8f01124ba623e
meliveloz <mvelozcampos@gmail.com>
2017-12-08 14:31:32 -0300

    litle change

--- asset/js/app.js
+++ asset/js/app.js
@@ -3,28 +3,44 @@ $(document).ready(function(){
 	$(function(){
    	setTimeout(function() {
       $('#splash').fadeOut(1000);
-   	}, 4000);
+   	}, 2000);
   });   // vista splash
 	
 
 
-  $("#user-filter").change(function() {    //usado para el filtrado con opciones
-    $("#galery").empty();  //cada vez que elijo una opción mi galeria borra lo anterior
-    var userOption = $("select").val(); //guardando en una variable el valor de mi elección 
-    for (var i = 0; i < foodPlace.length; i++) {   //recorriendo el largo de food place (mi data)
-      for(var j = 0; j < foodPlace[i].filters.length; j++) { //voy a recorrer mi array de filtros
-        if (foodPlace[i].filters[j] == userOption) { // si el valor de mi opción es igual a uno de mis filtros
-          var foodPhoto = foodPlace[i].photo; //guardo la foto de la coincidencia
-          var name=foodPlace[i].name; //guardo el nombre del restonant de la coincidencia
-          $("#galery").append("<div class='col-xs-4 photo-container'>"+foodPhoto+"</div>"); // agrego a mi galeria la foto del restorant.
-      
-        };
-      };
-    };
-    });
+ // $("galery").append("<a href='Akas-acapulco-sol-chair'><div class='product'><img src='img/AKAS.jpg' /><p class='producttitle'>Acapulco Sol</p><p class='productdesc>Chair</p></div></a>'");
+$("#user-filter").on("change",function(){
+//alert(this.value);
+  $("#galery").empty();
+  var prodsel=this.value;
+for(i=0 ; i< foodPlace.length; i++){
+  if(prodsel== foodPlace[i].filters)
+ $("#galery").append("<div class='col-xs-3 modal-click'>"+foodPlace[i].photo+"<p class='producttitle'>"+foodPlace[i].name+"</p><p class='productdesc>Chair</p></div>'");
 
+}
+
+$(".modal-click").click(function(){ //al hacer click en el contenedor de las fotos
+  $("#myModal").modal(); //iniciando el modal desde javascript
+  var thisPhoto = $(this).children("img").attr("alt"); // guardamos la variable del Alt de la foto al hacer click
+  for(i=0 ; i< foodPlace.length; i++){ //recorremos la data
+    if (thisPhoto == foodPlace[i].name) { // si el alt de la foto es igual al nombre entonces...
+      $(".modal-title").empty(); //borramos el titulo anterior
+      $(".modal-body").empty();// borramos el contenido del modal-body (la dirección);
+      $(".modal-title").html(foodPlace[i].name); // agregamos el nuevo nombre
+      
+     for (var n = 0; n < foodPlace[i].address.length; n++) { //recorremos la data en los array address
+      $(".modal-body").append("<p class='adress-modal'>"+ foodPlace[i].address[n] +"</p>"); //agregamos la nueva dirección
+     };
+
+    };
+
+};
+
+
+
+});
     
-     
+});     
 
 
 });
--- asset/js/data.js
+++ asset/js/data.js
@@ -1,51 +1,115 @@
 var foodPlace = [
   {
     name: "Sukine",
-    filters: ["korean", "vegetarian", "chinese"],
-    address: "Santa Filomena 2345",
-    photo: "<img src='asset/img/korean4.jpg' alt='Sukine' class='img-responsive' data-toggle='modal' data-target='#myModal'>"
+    filters: "korean",
+    address: ["Santa Filomena, 2345"],
+    photo: "<img src='asset/img/korean1.jpg' alt='Sukine' class='img-responsive'>"
    
   },
   {
     name: "Chinese Club",
-    filters: ["vegetarian", "bakery","chinese","korean"],
-    address: "Los trapenses 3456",
-    photo: "<img src='asset/img/chinese4.jpg' alt='Chinese Club' class='img-responsive' data-toggle='modal' data-target='#myModal' >"
+    filters: "chinese",
+    address: ["Los trapenses, 3456"],
+    photo: "<img src='asset/img/chinese1.jpg' alt='Chinese Club' class='img-responsive' >"
    
   },
   {
     name: "Eladio",
-    filters: ["vegetarian", "chinese", "barbacue","chilean"],
-    address: "Bellavista 567, Providencia",
-    photo: "<img src='asset/img/barbacue2.jpg' alt='Eladio' class='img-responsive' data-toggle='modal' data-target='#myModal'>"
+    filters: "barbacue",
+    address: ["Bellavista 567, Providencia"],
+    photo: "<img src='asset/img/barbacue1.jpg' alt='Eladio' class='img-responsive' >"
     
   },
   {
     name: "El Criollo",
-    filters: [ "chilean","barbacue"],
-    address: "Agustinas 2345, Santiago Centro",
-    photo: "<img src='asset/img/chilean1.jpg' alt='El Criollo' class='img-responsive' data-toggle='modal' data-target='#myModal'>"
+    filters: "chilean",
+    address: ["Agustinas 2345, Santiago Centro"],
+    photo: "<img src='asset/img/chilean1.jpg' alt='El Criollo' class='img-responsive tap'>"
     
   },
   {
     name: "El Centro",
-    filters: ["bakery", "vegetarian", "barbacue","chilean"],
-    address: "Huerfanos 2632, Santiago Centro",
-    photo: "<img src='asset/img/chilean6.jpg' alt='El Centro' class='img-responsive' data-toggle='modal' data-target='#myModal'>"
+    filters: "chilean",
+    address: ["Huerfanos 2632, Santiago Centro"],
+    photo: "<img src='asset/img/chilean2.jpg' alt='El Centro' class='img-responsive tap'>"
    
   },
   {
     name: "Hansoban",
-    filters: ["korean", "bakery","chinese","vegetarian"],
-    address: "Santa Filomena 893, Recoleta",
-    photo: "<img src='asset/img/chilean7.jpg' alt='Hansoban' class='img-responsive ' data-toggle='modal' data-target='#myModal'>"
+    filters: "korean",
+    address: ["Santa Filomena 893, Recoleta"],
+    photo: "<img src='asset/img/korean2.jpg' alt='Hansoban' class='img-responsive tap'>"
    
   },
   {
-    name: "Masita choza",
-    filters: ["chinese", "barbacue","chinese"],
-    address: "Av. Santa Maria 449",
-    photo: "<img src='asset/img/barbacue5.jpg' alt='Hansoban' class='img-responsive ' data-toggle='modal' data-target='#myModal'>"
+    name: "Masita Choza",
+    filters: "chinese",
+    address:["Av. Santa Maria 449"],
+    photo: "<img src='asset/img/chinese2.jpeg' alt='Masita Choza' class='img-responsive tap'>"
+   
+  },
+  {
+    name: "Parriladas Renco",
+    filters: "barbacue",
+    address: ["Huerfanos 2567, santiago Centro"],
+    photo: "<img src='asset/img/barbacue2.jpg' alt='Parrilladas Renco' class='img-responsive tap'>"
+   
+  },
+  {
+    name: "Barbacue Santi",
+    filters: "barbacue",
+    address: ["loreto 2345, santiago Centro"],
+    photo: "<img src='asset/img/barbacue3.jpg' alt='Barbacue Santi' class='img-responsive tap'>"
+   
+  },
+  {
+    name: "Parrilladas Argentinas",
+    filters: "barbacue",
+    address: ["loreto 2345, santiago Centro"],
+    photo: "<img src='asset/img/barbacue4.jpg' alt='Parrilladas Argentinas' class='img-responsive tap'>"
+   
+  },
+  {
+    name: "Kim Heelsun",
+    filters: "korean",
+    address: ["Santa Filomena 993, Recoleta"],
+    photo: "<img src='asset/img/korean4.jpg' alt='Kim Heelsun' class='img-responsive tap'>"
+   
+  },
+  {
+    name: "Seoul",
+    filters: "korean",
+    address: ["Santa Filomena 993, Recoleta"],
+    photo: "<img src='asset/img/korean3.jpg' alt='Seoul' class='img-responsive tap'>"
+   
+  },
+
+  {
+    name: "Chinese Master",
+    filters: "chinese",
+    address: ["Av. Santa Maria 723"],
+    photo: "<img src='asset/img/chinese3.jpg' alt='Chinese Master' class='img-responsive tap'>"
+   
+  },
+  {
+    name: "Tokyo Stop",
+    filters: "chinese",
+    address: ["Av. Santa Maria 723"],
+    photo: "<img src='asset/img/chinese4.jpg' alt='Tokyo Stop' class='img-responsive tap'>"
+   
+  },
+  {
+    name: "El Sureño",
+    filters: "chilean",
+    address: ["Monjitas 456, Santiago Centro"],
+    photo: "<img src='asset/img/chilean3.jpg' alt='El Sureño' class='img-responsive tap'>"
+   
+  },
+  {
+    name: "El Chicha",
+    filters: "chilean",
+    address: ["Monjitas, Santiago Centro"],
+    photo: "<img src='asset/img/chilean4.jpg' alt='El Chicha' class='img-responsive tap'>"
    
   }
 
