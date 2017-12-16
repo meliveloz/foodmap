a97cd590c2fec85941017aa66d69d876e0bd1947
meliveloz <mvelozcampos@gmail.com>
2017-12-15 22:52:32 -0300

    comment

--- asset/js/app.js
+++ asset/js/app.js
@@ -4,19 +4,19 @@ $(document).ready(function(){
    	setTimeout(function() {
       $('#splash').fadeOut(1000);
    	}, 4000);
-  });
+  });   // vista splash
 	
 
 
-  $("#user-filter").change(function() {
-    $("#galery").empty();
-    var userOption = $("select").val();
-    for (var i = 0; i < foodPlace.length; i++) {
-      for(var j = 0; j < foodPlace[i].filters.length; j++) {
-        if (foodPlace[i].filters[j] == userOption) {
-          var foodPhoto = foodPlace[i].photo;
-          var name=foodPlace[i].name;
-          $("#galery").append("<div class='col-xs-4 photo-container'>"+foodPhoto+"</div>");
+  $("#user-filter").change(function() {    //usado para el filtrado con opciones
+    $("#galery").empty();  //cada vez que elijo una opción mi galeria borra lo anterior
+    var userOption = $("select").val(); //guardando en una variable el valor de mi elección 
+    for (var i = 0; i < foodPlace.length; i++) {   //recorriendo el largo de food place (mi data)
+      for(var j = 0; j < foodPlace[i].filters.length; j++) { //voy a recorrer mi array de filtros
+        if (foodPlace[i].filters[j] == userOption) { // si el valor de mi opción es igual a uno de mis filtros
+          var foodPhoto = foodPlace[i].photo; //guardo la foto de la coincidencia
+          var name=foodPlace[i].name; //guardo el nombre del restonant de la coincidencia
+          $("#galery").append("<div class='col-xs-4 photo-container'>"+foodPhoto+"</div>"); // agrego a mi galeria la foto del restorant.
       
         };
       };
